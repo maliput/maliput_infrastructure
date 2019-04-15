a4a77c3131924384ae7cbb6b20b70f573a2872fc
Michel Hidalgo <michel@ekumenlabs.com>
2019-04-15 08:04:13 -0700

    Freeze drake on workspace setup scripts too (#17)
    * Make workspace setup scripts stick to frozen drake version.
    
    Signed-off-by: Michel Hidalgo <michel@ekumenlabs.com>
    
    * Deduplicate CI and workspace setup scripts.
    
    Signed-off-by: Michel Hidalgo <michel@ekumenlabs.com>
    
    * Fix unbound PS1 in workspace .bashrc
    
    Signed-off-by: Michel Hidalgo <michel@ekumenlabs.com>
    
    * Do not fail CI build on unbound variable.
    
    Signed-off-by: Michel Hidalgo <michel@ekumenlabs.com>
    
    * Move merge back action into setup script.
    
    Signed-off-by: Michel Hidalgo <michel@ekumenlabs.com>
    
    * Move merge back action outside setup script as a user defined hook.
    
    Signed-off-by: Michel Hidalgo <michel@ekumenlabs.com>
    
    * Force repo cloning.
    
    Signed-off-by: Michel Hidalgo <michel@ekumenlabs.com>

--- ci/jenkins/pipeline.groovy
+++ ci/jenkins/pipeline.groovy
@@ -1,11 +1,8 @@
-stage('setup early') {
-   sh './index/ci/jenkins/setup_early'
+stage('setup environment') {
+   sh './index/ci/jenkins/setup_environment'
 }
 stage('build workspace') {
    sh './index/ci/jenkins/build_workspace'
-}
-stage('setup late') {
-   sh './index/ci/jenkins/setup_late'
 }
 stage('build') {
    sh './index/ci/jenkins/build'
