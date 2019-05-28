8da46c2d9ea3af9f09379393a752a48458903c86
Michel Hidalgo <michel@ekumenlabs.com>
2019-05-28 15:10:18 -0300

    Adapt CI scripts to new dsim workspace tooling.
    Signed-off-by: Michel Hidalgo <michel@ekumenlabs.com>

--- ci/jenkins/pipeline.groovy
+++ ci/jenkins/pipeline.groovy
@@ -1,12 +1,12 @@
-stage('setup environment') {
-   sh './index/ci/jenkins/setup_environment'
+stage('setup workspace') {
+   sh './index/ci/jenkins/setup_ws'
 }
 stage('build workspace') {
-   sh './index/ci/jenkins/build_workspace'
+   sh './index/ci/jenkins/ws ./index/ci/jenkins/build_ws'
 }
 stage('build') {
-   sh './index/ci/jenkins/build'
+   sh './index/ci/jenkins/ws ./index/ci/jenkins/build'
 }
 stage('test') {
-   sh './index/ci/jenkins/test'
+   sh './index/ci/jenkins/ws ./index/ci/jenkins/test'
 }
