261a24c1bd0d4474b0e845b69c4eab36e2826c33
Michel Hidalgo <michel@ekumenlabs.com>
2019-10-21 13:35:30 -0300

    Build and publish documentation nightly.
    Signed-off-by: Michel Hidalgo <michel@ekumenlabs.com>

--- cd/jenkins/pipeline.groovy
+++ cd/jenkins/pipeline.groovy
@@ -1,10 +1,12 @@
-load './index/ci/jenkins/pipeline.groovy'
-
+withEnv(['COLCON_BUILD_EXTRA_ARGS=${env.COLCON_BUILD_EXTRA_ARGS} --cmake-args -DBUILD_DOCS=1']) {
+  load './index/ci/jenkins/pipeline.groovy'
+}
 stage('deploy') {
+  sh './index/cd/jenkins/publish_docs'
   withCredentials([[$class: 'AmazonWebServicesCredentialsBinding',
                   credentialsId: 'c424c772-1817-4b13-9fe0-6ed2f22c0576',
                   accessKeyVariable: 'AWS_ACCESS_KEY_ID',
                   secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
-    sh './index/cd/jenkins/deploy'
+    sh './index/cd/jenkins/deploy_binaries'
   }
 }
