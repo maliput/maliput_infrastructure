dc8fa4c5d6d850ebb66c0988db70000ba960da48
Michel Hidalgo <michel@ekumenlabs.com>
2019-10-21 13:36:50 -0300

    Test pipeline
    Signed-off-by: Michel Hidalgo <michel@ekumenlabs.com>

--- cd/jenkins/pipeline.groovy
+++ cd/jenkins/pipeline.groovy
@@ -3,10 +3,4 @@ withEnv(['COLCON_BUILD_EXTRA_ARGS=${env.COLCON_BUILD_EXTRA_ARGS} --cmake-args -D
 }
 stage('deploy') {
   sh './index/cd/jenkins/publish_docs'
-  withCredentials([[$class: 'AmazonWebServicesCredentialsBinding',
-                  credentialsId: 'c424c772-1817-4b13-9fe0-6ed2f22c0576',
-                  accessKeyVariable: 'AWS_ACCESS_KEY_ID',
-                  secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
-    sh './index/cd/jenkins/deploy_binaries'
-  }
 }
