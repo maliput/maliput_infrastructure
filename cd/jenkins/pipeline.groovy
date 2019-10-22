6d686433cd03424057ac5cdd048ff4b5bb0920d0
Michel Hidalgo <michel@ekumenlabs.com>
2019-10-22 10:21:56 -0300

    Use double quotes for string interpolation.
    Signed-off-by: Michel Hidalgo <michel@ekumenlabs.com>

--- cd/jenkins/pipeline.groovy
+++ cd/jenkins/pipeline.groovy
@@ -1,4 +1,4 @@
-withEnv(['COLCON_BUILD_EXTRA_ARGS=${env.COLCON_BUILD_EXTRA_ARGS} --cmake-args -DBUILD_DOCS=1']) {
+withEnv(["COLCON_BUILD_EXTRA_ARGS=${env.COLCON_BUILD_EXTRA_ARGS} --cmake-args -DBUILD_DOCS=1"]) {
   load './index/ci/jenkins/pipeline.groovy'
 }
 stage('deploy') {
