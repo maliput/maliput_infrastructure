dc1e81fd21a07cd4b843f25d4641981cfe84a352
Agustin Alba Chicar <ag.albachicar@ekumenlabs.com>
2020-11-17 12:07:52 -0300

    Restores CD in dsim-repos-index (#144)
    Copies from CI the basic build steps to generate the tarballs.

--- cd/jenkins/pipeline.groovy
+++ cd/jenkins/pipeline.groovy
@@ -1,5 +1,14 @@
-load './index/ci/jenkins/pipeline.groovy'
-
+stage('setup') {
+   sh './index/ci/jenkins/setup_workspace'
+   sh './index/ci/jenkins/ws ./index/ci/jenkins/pull_sources'
+   sh './index/ci/jenkins/ws ./index/ci/jenkins/install_deps'
+}
+stage('build') {
+   sh './index/ci/jenkins/ws ./index/ci/jenkins/build'
+}
+stage('test') {
+   sh './index/ci/jenkins/ws ./index/ci/jenkins/test'
+}
 stage('deploy') {
   withCredentials([[$class: 'AmazonWebServicesCredentialsBinding',
                   credentialsId: 'c424c772-1817-4b13-9fe0-6ed2f22c0576',
