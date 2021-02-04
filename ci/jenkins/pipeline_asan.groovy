ee7f259f8731d3799bca728019215b262e0f4813
Agustin Alba Chicar <ag.albachicar@ekumenlabs.com>
2021-02-04 10:10:35 -0300

    Removes Jenkins configuration

--- ci/jenkins/pipeline_asan.groovy
+++
@@ -1,8 +0,0 @@
-stage('[asan]setup') {
-   sh './index/ci/jenkins/setup_workspace'
-   sh './index/ci/jenkins/ws ./index/ci/jenkins/pull_sources'
-   sh './index/ci/jenkins/ws ./index/ci/jenkins/install_deps'
-}
-stage('[asan]address_sanitizer') {
-   sh './index/ci/jenkins/ws ./index/ci/jenkins/address_sanitizer'
-}
--- ci/jenkins/pipeline_scan_build.groovy
+++
@@ -1,8 +0,0 @@
-stage('[scan_build]setup') {
-   sh './index/ci/jenkins/setup_workspace'
-   sh './index/ci/jenkins/ws ./index/ci/jenkins/pull_sources'
-   sh './index/ci/jenkins/ws ./index/ci/jenkins/install_deps'
-}
-stage('[scan_build]static analyzer') {
-   sh './index/ci/jenkins/ws ./index/ci/jenkins/scan_build'
-}
