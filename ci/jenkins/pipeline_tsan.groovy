9387281e8b34c6436cf861c965b26ade95963e19
Agustin Alba Chicar <ag.albachicar@ekumenlabs.com>
2021-02-04 11:28:43 -0300

    Removes Jenkins configuration (#163)

--- ci/jenkins/pipeline_tsan.groovy
+++
@@ -1,8 +0,0 @@
-stage('[tsan]setup') {
-   sh './index/ci/jenkins/setup_workspace'
-   sh './index/ci/jenkins/ws ./index/ci/jenkins/pull_sources'
-   sh './index/ci/jenkins/ws ./index/ci/jenkins/install_deps'
-}
-stage('[tsan]thread_sanitizer') {
-   sh './index/ci/jenkins/ws ./index/ci/jenkins/thread_sanitizer'
-}
